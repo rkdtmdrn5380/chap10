c4b2850a873a01c7b9598ee399317015e69ed218
JAVA <JAVA>
2017-02-23 10:25:29 +0900

    settings

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,6 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
 
 @SpringBootApplication
 public class DemoApplication {
